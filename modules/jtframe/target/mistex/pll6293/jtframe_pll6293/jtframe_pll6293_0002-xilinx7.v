1fb7006919720e1677a094946e5b1aaca00ac51e
Hans Baier <foss@hans-baier.de>
2024-04-17 05:43:45 +0700

    implement mistex target, many thanks to @somhi for porting it

---
+++ modules/jtframe/target/mistex/pll6293/jtframe_pll6293/jtframe_pll6293_0002-xilinx7.v
@@ -0,0 +1,242 @@
+
+// file: pll_0002.v
+// 
+// (c) Copyright 2008 - 2013 Xilinx, Inc. All rights reserved.
+// 
+// This file contains confidential and proprietary information
+// of Xilinx, Inc. and is protected under U.S. and
+// international copyright and other intellectual property
+// laws.
+// 
+// DISCLAIMER
+// This disclaimer is not a license and does not grant any
+// rights to the materials distributed herewith. Except as
+// otherwise provided in a valid license issued to you by
+// Xilinx, and to the maximum extent permitted by applicable
+// law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+// WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+// AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+// BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+// INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and
+// (2) Xilinx shall not be liable (whether in contract or tort,
+// including negligence, or under any other theory of
+// liability) for any loss or damage of any kind or nature
+// related to, arising under or in connection with these
+// materials, including for any direct, or any indirect,
+// special, incidental, or consequential loss or damage
+// (including loss of data, profits, goodwill, or any type of
+// loss or damage suffered as a result of any action brought
+// by a third party) even if such damage or loss was
+// reasonably foreseeable or Xilinx had been advised of the
+// possibility of the same.
+// 
+// CRITICAL APPLICATIONS
+// Xilinx products are not designed or intended to be fail-
+// safe, or for use in any application requiring fail-safe
+// performance, such as life-support or safety devices or
+// systems, Class III medical devices, nuclear facilities,
+// applications related to the deployment of airbags, or any
+// other applications that could lead to death, personal
+// injury, or severe property or environmental damage
+// (individually and collectively, "Critical
+// Applications"). Customer assumes the sole risk and
+// liability of any use of Xilinx products in Critical
+// Applications, subject only to applicable laws and
+// regulations governing limitations on product liability.
+// 
+// THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS
+// PART OF THIS FILE AT ALL TIMES.
+// 
+//----------------------------------------------------------------------------
+// User entered comments
+//----------------------------------------------------------------------------
+// None
+//
+//----------------------------------------------------------------------------
+//  Output     Output      Phase    Duty Cycle   Pk-to-Pk     Phase
+//   Clock     Freq (MHz)  (degrees)    (%)     Jitter (ps)  Error (ps)
+//----------------------------------------------------------------------------
+// outclk_0__50.34722______0.000______50.0______494.485____407.876
+// outclk_1__50.34722_____90.000______50.0______494.485____407.876
+// outclk_2__25.17361______0.000______50.0______567.110____407.876
+// outclk_3___6.29340______0.000______50.0______728.141____407.876
+// outclk_4__100.69444______0.000______50.0______423.366____407.876
+// outclk_5__100.69444____-180.000______50.0______423.366____407.876
+//
+//----------------------------------------------------------------------------
+// Input Clock   Freq (MHz)    Input Jitter (UI)
+//----------------------------------------------------------------------------
+// __primary__________50.000____________0.010
+
+`timescale 1ps/1ps
+
+module jtframe_pll6293_0002 
+
+ (// Clock in ports
+ input         refclk,
+
+  // Clock out ports
+  output        outclk_0,
+  output        outclk_1,
+  output        outclk_2,
+  output        outclk_3,
+  output        outclk_4,
+  output        outclk_5,
+  // Status and control signals
+  input         rst,
+  output        locked
+ );
+  // Input buffering
+  //------------------------------------
+wire refclk_pll_0002;
+wire clk_in2_pll_0002;
+  IBUF clkin1_ibufg
+   (.O (refclk_pll_0002),
+    .I (refclk));
+
+
+  // Clocking PRIMITIVE
+  //------------------------------------
+
+  // Instantiation of the MMCM PRIMITIVE
+  //    * Unused inputs are tied off
+  //    * Unused outputs are labeled unused
+
+  wire        outclk_0_pll_0002;
+  wire        outclk_1_pll_0002;
+  wire        outclk_2_pll_0002;
+  wire        outclk_3_pll_0002;
+  wire        outclk_4_pll_0002;
+  wire        outclk_5_pll_0002;
+  wire        clk_out7_pll_0002;
+
+  wire [15:0] do_unused;
+  wire        drdy_unused;
+  wire        psdone_unused;
+  wire        locked_int;
+  wire        clkfbout_pll_0002;
+  wire        clkfbout_buf_pll_0002;
+  wire        clkfboutb_unused;
+    wire clkout0b_unused;
+   wire clkout1b_unused;
+   wire clkout2b_unused;
+   wire clkout3b_unused;
+  wire        clkout6_unused;
+  wire        clkfbstopped_unused;
+  wire        clkinstopped_unused;
+  wire        reset_high;
+
+  MMCME2_ADV
+  #(.BANDWIDTH            ("OPTIMIZED"),
+    .CLKOUT4_CASCADE      ("FALSE"),
+    .COMPENSATION         ("ZHOLD"),
+    .STARTUP_WAIT         ("FALSE"),
+    .DIVCLK_DIVIDE        (3),
+    .CLKFBOUT_MULT_F      (36.250),
+    .CLKFBOUT_PHASE       (0.000),
+    .CLKFBOUT_USE_FINE_PS ("FALSE"),
+    .CLKOUT0_DIVIDE_F     (12.000),
+    .CLKOUT0_PHASE        (0.000),
+    .CLKOUT0_DUTY_CYCLE   (0.500),
+    .CLKOUT0_USE_FINE_PS  ("FALSE"),
+    .CLKOUT1_DIVIDE       (12),
+    .CLKOUT1_PHASE        (90.000),
+    .CLKOUT1_DUTY_CYCLE   (0.500),
+    .CLKOUT1_USE_FINE_PS  ("FALSE"),
+    .CLKOUT2_DIVIDE       (24),
+    .CLKOUT2_PHASE        (0.000),
+    .CLKOUT2_DUTY_CYCLE   (0.500),
+    .CLKOUT2_USE_FINE_PS  ("FALSE"),
+    .CLKOUT3_DIVIDE       (96),
+    .CLKOUT3_PHASE        (0.000),
+    .CLKOUT3_DUTY_CYCLE   (0.500),
+    .CLKOUT3_USE_FINE_PS  ("FALSE"),
+    .CLKOUT4_DIVIDE       (6),
+    .CLKOUT4_PHASE        (0.000),
+    .CLKOUT4_DUTY_CYCLE   (0.500),
+    .CLKOUT4_USE_FINE_PS  ("FALSE"),
+    .CLKOUT5_DIVIDE       (6),
+    .CLKOUT5_PHASE        (-180.000),
+    .CLKOUT5_DUTY_CYCLE   (0.500),
+    .CLKOUT5_USE_FINE_PS  ("FALSE"),
+    .CLKIN1_PERIOD        (20.000))
+  mmcm_adv_inst
+    // Output clocks
+   (
+    .CLKFBOUT            (clkfbout_pll_0002),
+    .CLKFBOUTB           (clkfboutb_unused),
+    .CLKOUT0             (outclk_0_pll_0002),
+    .CLKOUT0B            (clkout0b_unused),
+    .CLKOUT1             (outclk_1_pll_0002),
+    .CLKOUT1B            (clkout1b_unused),
+    .CLKOUT2             (outclk_2_pll_0002),
+    .CLKOUT2B            (clkout2b_unused),
+    .CLKOUT3             (outclk_3_pll_0002),
+    .CLKOUT3B            (clkout3b_unused),
+    .CLKOUT4             (outclk_4_pll_0002),
+    .CLKOUT5             (outclk_5_pll_0002),
+    .CLKOUT6             (clkout6_unused),
+     // Input clock control
+    .CLKFBIN             (clkfbout_buf_pll_0002),
+    .CLKIN1              (refclk_pll_0002),
+    .CLKIN2              (1'b0),
+     // Tied to always select the primary input clock
+    .CLKINSEL            (1'b1),
+    // Ports for dynamic reconfiguration
+    .DADDR               (7'h0),
+    .DCLK                (1'b0),
+    .DEN                 (1'b0),
+    .DI                  (16'h0),
+    .DO                  (do_unused),
+    .DRDY                (drdy_unused),
+    .DWE                 (1'b0),
+    // Ports for dynamic phase shift
+    .PSCLK               (1'b0),
+    .PSEN                (1'b0),
+    .PSINCDEC            (1'b0),
+    .PSDONE              (psdone_unused),
+    // Other control and status signals
+    .LOCKED              (locked_int),
+    .CLKINSTOPPED        (clkinstopped_unused),
+    .CLKFBSTOPPED        (clkfbstopped_unused),
+    .PWRDWN              (1'b0),
+    .RST                 (reset_high));
+  assign reset_high = rst; 
+
+  assign locked = locked_int;
+// Clock Monitor clock assigning
+//--------------------------------------
+ // Output buffering
+  //-----------------------------------
+
+  BUFG clkf_buf
+   (.O (clkfbout_buf_pll_0002),
+    .I (clkfbout_pll_0002));
+
+
+  BUFG clkout1_buf
+   (.O   (outclk_0),
+    .I   (outclk_0_pll_0002));
+
+
+  BUFG clkout2_buf
+   (.O   (outclk_1),
+    .I   (outclk_1_pll_0002));
+
+  BUFG clkout3_buf
+   (.O   (outclk_2),
+    .I   (outclk_2_pll_0002));
+
+  BUFG clkout4_buf
+   (.O   (outclk_3),
+    .I   (outclk_3_pll_0002));
+
+  BUFG clkout5_buf
+   (.O   (outclk_4),
+    .I   (outclk_4_pll_0002));
+
+  BUFG clkout6_buf
+   (.O   (outclk_5),
+    .I   (outclk_5_pll_0002));
+
+endmodule
